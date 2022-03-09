840bc1dc175202fcb4db3317f82f23cd2a15d7d9
nichtl <43055414+nichtl@users.noreply.github.com>
2022-03-09 19:07:12 +0800

    Update BookScanner.java

--- src/com/nicht/fishbook/service/BookScanner.java
+++ src/com/nicht/fishbook/service/BookScanner.java
@@ -2,11 +2,7 @@ package com.nicht.fishbook.service;
 
 import java.util.List;
 
-/**
- * @Author jogeen
- * @Date 14:28 2020/6/24
- * @Description
- */
+
 public interface BookScanner {
      String bookName();
      long getBookSize();
